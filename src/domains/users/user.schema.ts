f44be826da3e75f7cb91f78d2c1e9e744ffb8b8b
BhagabanGhadai <bhagabanghadai043@gmail.com>
2026-08-08 23:48:54 +0530

    feat: Add zod dependency and implement user validation schemas

---
+++ src/domains/users/user.schema.ts
@@ -0,0 +1,31 @@
+import * as z from 'zod';
+import { ECurrentStatus } from './user.interface';
+
+export const createUserSchema = z.object({
+    mobile: z.string().min(10, "Mobile number must be at least 10 digits long"),
+});
+
+export const updateUserSchema = z.object({
+    name: z.string().min(3, "Name must be at least 3 characters long").optional(),
+    email: z.string().email("Invalid email address").optional(),
+    avatar: z.string().optional(),
+});
+
+export const getUserSchema = z.object({
+    currentStatus: z.enum(ECurrentStatus).optional(),
+    search: z.string().optional(),
+    page: z.number().optional(),
+    limit: z.number().optional(),
+}).refine((data)=>{
+    if(data.page && !data.limit){
+        data.limit = 10;
+    }
+    if(data.limit && !data.page){
+        data.page = 1;
+    }
+    return true;
+})
+
+export type CreateUserSchema = z.infer<typeof createUserSchema>;
+export type UpdateUserSchema = z.infer<typeof updateUserSchema>;
+export type GetUserSchema = z.infer<typeof getUserSchema>;
