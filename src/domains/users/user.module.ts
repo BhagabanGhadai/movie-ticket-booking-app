e8efd1b2ea5fd48124007ebd813631f574be5492
BhagabanGhadai <bhagabanghadai043@gmail.com>
2026-08-08 23:59:01 +0530

    feat: Register UserService and UserController in UserModule

--- src/domains/users/user.module.ts
+++ src/domains/users/user.module.ts
@@ -2,10 +2,12 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { User, UserSchema } from "./entities/user";
 import { UserRepository } from "./user.repository";
+import { UserService } from "./user.service";
+import { UserController } from "./user.controller";
 
 @Module({
     imports:[MongooseModule.forFeature([{name:User.name,schema:UserSchema}])],
-    providers:[UserRepository],
+    providers:[UserRepository,UserService,UserController],
     exports:[UserRepository]
 })
 export class UserModule {}
