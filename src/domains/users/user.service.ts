9c441ab5c2d49eeddbfd93874ae121b90f401b3f
BhagabanGhadai <bhagabanghadai043@gmail.com>
2026-08-09 13:27:09 +0530

    feat: implement user service methods for create, read, and update operations

--- src/domains/users/user.service.ts
+++ src/domains/users/user.service.ts
@@ -4,21 +4,21 @@ import { CreateUserSchema, GetAllUserSchema, IdSchema, UpdateUserSchema } from "
 
 export class UserService{
     constructor(private readonly userRepository:UserRepository){}
-    
-    async createUser(createUserSchema:CreateUserSchema){
 
+    async createUser(createUserDto:CreateUserSchema){
+        return await this.userRepository.create(createUserDto)
     }
 
-    async getAllUser(getUserSchema:GetAllUserSchema){
-
+    async getAllUser(getUserDto:GetAllUserSchema){
+        return await this.userRepository.getAll(getUserDto)
     }
 
     async getUser(idDto:IdSchema){
         return await this.userRepository.getOne(idDto)
     }
 
-    async updateUser(updateUserSchema:UpdateUserSchema){
-        
+    async updateUser(updateUserDto:UpdateUserSchema){
+        return await this.userRepository.updateOne({_id:updateUserDto._id},updateUserDto)
     }
 
     async deleteUser(idDto:IdSchema){
