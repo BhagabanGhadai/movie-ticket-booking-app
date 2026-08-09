9cb7b7f27ac68e008989cb8b57ea8724be1f179e
BhagabanGhadai <bhagabanghadai043@gmail.com>
2026-08-09 14:28:52 +0530

    refactor: add explicit return types to UserService and UserController methods using UserDocument

--- src/domains/users/user.controller.ts
+++ src/domains/users/user.controller.ts
@@ -3,6 +3,7 @@ import { UserService } from "./user.service";
 import { createUserSchema, CreateUserSchema, GetAllUserSchema, UpdateUserSchema,getAllUserSchema,updateUserSchema, idSchema, IdSchema} from "./user.schema";
 import { Types } from "mongoose";
 import { ZodValidationPipe } from "src/core/pipes";
+import { UserDocument } from "./user.entity";
 
 @Controller("user")
 export class UserController{
@@ -11,35 +12,35 @@ export class UserController{
     @Post()
     @HttpCode(201)
     @UsePipes(new ZodValidationPipe(createUserSchema))
-    async createUser(@Body() createUserDto:CreateUserSchema){
+    async createUser(@Body() createUserDto:CreateUserSchema):Promise<UserDocument>{
         return this.userService.createUser(createUserDto);
     }
 
     @Get()
     @HttpCode(200)
     @UsePipes(new ZodValidationPipe(getAllUserSchema))
-    async getAllUser(@Body() getUserDto:GetAllUserSchema){
+    async getAllUser(@Body() getUserDto:GetAllUserSchema):Promise<UserDocument[]>{
         return this.userService.getAllUser(getUserDto);
     }
 
     @Get(":_id")
     @HttpCode(200)
     @UsePipes(new ZodValidationPipe(idSchema))
-    async getUser(@Param() idDto: IdSchema){
+    async getUser(@Param() idDto: IdSchema):Promise<UserDocument | null>{
         return this.userService.getUser(idDto);
     }
 
     @Patch()
     @HttpCode(200)
     @UsePipes(new ZodValidationPipe(updateUserSchema))
-    async updateUser(@Body() updateUserDto:UpdateUserSchema){
+    async updateUser(@Body() updateUserDto:UpdateUserSchema):Promise<UserDocument | null>{
         return this.userService.updateUser(updateUserDto);
     }
 
     @Delete(":_id")
     @HttpCode(200)
     @UsePipes(new ZodValidationPipe(idSchema))
-    async deleteUser(@Param() idDto: IdSchema){
+    async deleteUser(@Param() idDto: IdSchema):Promise<UserDocument | null>{
         return this.userService.deleteUser(idDto);
     }
 }
--- src/domains/users/user.service.ts
+++ src/domains/users/user.service.ts
@@ -1,27 +1,28 @@
 import { Types } from "mongoose";
 import { UserRepository } from "./user.repository";
 import { CreateUserSchema, GetAllUserSchema, IdSchema, UpdateUserSchema } from "./user.schema";
+import { UserDocument } from "./user.entity";
 
 export class UserService{
     constructor(private readonly userRepository:UserRepository){}
 
-    async createUser(createUserDto:CreateUserSchema){
+    async createUser(createUserDto:CreateUserSchema):Promise<UserDocument>{
         return await this.userRepository.create(createUserDto)
     }
 
-    async getAllUser(getUserDto:GetAllUserSchema){
+    async getAllUser(getUserDto:GetAllUserSchema):Promise<UserDocument[]>{
         return await this.userRepository.getAll(getUserDto)
     }
 
-    async getUser(idDto:IdSchema){
+    async getUser(idDto:IdSchema):Promise<UserDocument | null>{
         return await this.userRepository.getOne(idDto)
     }
 
-    async updateUser(updateUserDto:UpdateUserSchema){
+    async updateUser(updateUserDto:UpdateUserSchema):Promise<UserDocument | null>{
         return await this.userRepository.updateOne({_id:updateUserDto._id},updateUserDto)
     }
 
-    async deleteUser(idDto:IdSchema){
+    async deleteUser(idDto:IdSchema):Promise<UserDocument | null>{
         return await this.userRepository.softDelete(idDto);
     }
 }
