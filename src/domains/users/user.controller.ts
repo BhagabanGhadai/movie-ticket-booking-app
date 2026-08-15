8fa7d7bf33a8e6cad9dba4cc4d83d094c4fe7b7c
BhagabanGhadai <bhagabanghadai043@gmail.com>
2026-08-15 12:44:47 +0530

    refactor: Add generic type parameters to ZodValidationPipe instances in user controller

--- src/domains/users/user.controller.ts
+++ src/domains/users/user.controller.ts
@@ -11,35 +11,35 @@ export class UserController{
     
     @Post()
     @HttpCode(201)
-    @UsePipes(new ZodValidationPipe(createUserSchema))
+    @UsePipes(new ZodValidationPipe<CreateUserSchema>(createUserSchema))
     async createUser(@Body() createUserDto:CreateUserSchema):Promise<UserDocument>{
         return this.userService.createUser(createUserDto);
     }
 
     @Get()
     @HttpCode(200)
-    @UsePipes(new ZodValidationPipe(getAllUserSchema))
+    @UsePipes(new ZodValidationPipe<GetAllUserSchema>(getAllUserSchema))
     async getAllUser(@Body() getUserDto:GetAllUserSchema):Promise<UserDocument[]>{
         return this.userService.getAllUser(getUserDto);
     }
 
     @Get(":_id")
     @HttpCode(200)
-    @UsePipes(new ZodValidationPipe(idSchema))
+    @UsePipes(new ZodValidationPipe<IdSchema>(idSchema))
     async getUser(@Param() idDto: IdSchema):Promise<UserDocument | null>{
         return this.userService.getUser(idDto);
     }
 
     @Patch()
     @HttpCode(200)
-    @UsePipes(new ZodValidationPipe(updateUserSchema))
+    @UsePipes(new ZodValidationPipe<UpdateUserSchema>(updateUserSchema))
     async updateUser(@Body() updateUserDto:UpdateUserSchema):Promise<UserDocument | null>{
         return this.userService.updateUser(updateUserDto);
     }
 
     @Delete(":_id")
     @HttpCode(200)
-    @UsePipes(new ZodValidationPipe(idSchema))
+    @UsePipes(new ZodValidationPipe<IdSchema>(idSchema))
     async deleteUser(@Param() idDto: IdSchema):Promise<UserDocument | null>{
         return this.userService.deleteUser(idDto);
     }
