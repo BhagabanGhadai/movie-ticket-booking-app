c3100894529e2b43b77ebe9a543bab8db8d60a78
BhagabanGhadai <bhagabanghadai043@gmail.com>
2026-08-15 18:25:36 +0530

    feat: add auth guard and implement validation enhancements to user schemas using mongoose object ID validation and string trimming

--- src/domains/users/user.schema.ts
+++ src/domains/users/user.schema.ts
@@ -1,23 +1,23 @@
 import * as z from 'zod';
 import { ECurrentStatus } from './user.interface';
-import { Types } from 'mongoose';
+import { isValidObjectId, Types } from 'mongoose';
 
 export const createUserSchema = z.object({
-    mobile: z.string().min(10, "Mobile number must be at least 10 digits long"),
+    mobile: z.string().trim().min(10, "Mobile number must be at least 10 digits long"),
 });
 
 export const updateUserSchema = z.object({
-    _id: z.string().transform((id) => new Types.ObjectId(id)),
-    name: z.string().min(3, "Name must be at least 3 characters long").optional(),
-    email: z.string().email("Invalid email address").optional(),
-    avatar: z.string().optional(),
+    _id: z.string().refine(isValidObjectId, "Invalid ObjectId").transform((id) => new Types.ObjectId(id)),
+    name: z.string().trim().min(3, "Name must be at least 3 characters long").optional(),
+    email: z.string().trim().email("Invalid email address").optional(),
+    avatar: z.string().trim().optional(),
 });
 
 export const getAllUserSchema = z.object({
     currentStatus: z.enum(ECurrentStatus).optional(),
-    search: z.string().optional(),
-    page: z.number().optional(),
-    limit: z.number().optional(),
+    search: z.string().trim().optional(),
+    page: z.number().int().positive().optional(),
+    limit: z.number().int().positive().optional(),
 }).refine((data)=>{
     if(data.page && !data.limit){
         data.limit = 10;
@@ -29,7 +29,7 @@ export const getAllUserSchema = z.object({
 })
 
 export const idSchema = z.object({
-    _id: z.string().transform((id) => new Types.ObjectId(id)),
+    _id: z.string().refine(isValidObjectId, "Invalid ObjectId").transform((id) => new Types.ObjectId(id)),
 });
 
 export type CreateUserSchema = z.infer<typeof createUserSchema>;
