4d23d8c1d8f3df502caf7841c8de97dab8b25a40
BhagabanGhadai <bhagabanghadai043@gmail.com>
2026-08-15 11:50:47 +0530

    feat: register global exception filter in application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { GlobalExceptionFilter } from './core/filters/global.filter.exception';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule,new FastifyAdapter());
+  app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
